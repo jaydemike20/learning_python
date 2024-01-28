c2f02c9b145adef61b7f4e86af3f48368411839c
jaydemike21 <jaydemike21@gmail.com>
2024-01-28 23:02:11 +0800

    Python - Variables, Naming variables, Casting Data

---
+++ variables/variables.py
@@ -0,0 +1,22 @@
+# this is comment
+
+# data types
+x = 4 # integer
+y = 4.5 # float
+z = "Hello World" # string
+
+# casting data
+a = int(y) # float to integer
+b = float(x) # integer to float
+c = str(x) # integer to string
+
+print(a, b, c) #display/output
+
+
+# Type of Naming Variables
+# camelCase
+myFirstName = 'Jayde Mike'
+# pascalCase
+MyMiddleName = ''
+
+
